62540b490758d4db66b8dfb14d984d5f2130b582
MohamadAboud <mohamadaboud663@gmail.com>
2023-10-05 17:02:49 +0300

    update ip address

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ class Config:
     TOPIC_NAME: str = 'car_detection_topic'
 
     # Kafka bootstrap server configuration
-    BOOTSTRAP_IP: str = 'localhost'
+    BOOTSTRAP_IP: str = '10.104.93.100'
     BOOTSTRAP_PORT: str = '9092'
     BOOTSTRAP_SERVERS: List[str] = [f'{BOOTSTRAP_IP}:{BOOTSTRAP_PORT}']
